26a458970e8c506a30bd6fbed056d21d03d0d92e
TH Chan <th.chan@accenture.com>
2018-04-12 11:45:29 +0800

    Remove storyboard

--- SearchBookApp/AppDelegate.swift
+++ SearchBookApp/AppDelegate.swift
@@ -16,6 +16,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        window = UIWindow(frame:UIScreen.main.bounds)
+        if let window = self.window {
+            let landingViewController = LandingViewController()
+            window.rootViewController = landingViewController
+            window.makeKeyAndVisible()
+        }
+        
         return true
     }
 
---
+++ SearchBookApp/LandingViewController.swift
@@ -0,0 +1,36 @@
+//
+//  LandingViewController.swift
+//  SearchBookApp
+//
+//  Created by Chan, Tsz Hin on 12/4/2018.
+//  Copyright © 2018 temp. All rights reserved.
+//
+
+import UIKit
+
+class LandingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        print("Hello Landing")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
